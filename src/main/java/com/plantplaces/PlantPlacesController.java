39528e9a4c7618f141bdfaea5d6bb5a63d333ee2
Administrator <Administrator@DESKTOP-099UQRG>
2020-10-11 22:44:37 -0400

    Plantplaces application

--- src/main/java/com/plantplaces/PlantPlacesController.java
+++ src/main/java/com/plantplaces/PlantPlacesController.java
@@ -1,7 +1,15 @@
 package com.plantplaces;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.plantplaces.dto.SpecimenDTO;
+import com.plantplaces.service.ISpecimenService;
 
 /**
  * @author Administrator
@@ -11,13 +19,47 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class PlantPlacesController {
 	
+	@Autowired
+	private ISpecimenService specimenServiceStub;
+	private SpecimenDTO specimenDTO;
 	
-	@RequestMapping("/start")
-	public String Start(){
+	
+	@RequestMapping(value ="/start", method =RequestMethod.GET)
+	public String read(Model model){
+		SpecimenDTO specimenDTO = specimenServiceStub.fetchById(43);
+		model.addAttribute("specimenDTO", specimenDTO);
 		
 		return "start";
 	}
 	
+	@RequestMapping(value ="/start", method =RequestMethod.GET, headers= {"content-type=text/json"})
+	public String readJSON(){
+		
+		return "start";
+	}
+	
+	@RequestMapping(value ="/start", method =RequestMethod.GET, params= {"loyalty=blue"})
+	public String readBlue(){
+		
+		return "start";
+	}
+	
+	@RequestMapping(value ="/start", method =RequestMethod.GET, params={"loyalty=silver"})
+	public ModelAndView readSilver(){
+		SpecimenDTO specimenDTO = specimenServiceStub.fetchById(43);
+		specimenDTO.setSpecimenId(90);
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("start");
+		modelAndView.addObject("specimenDTO", specimenDTO);
+		return modelAndView;
+	}
+	
+	
+	@PostMapping("/start")
+	public String create(){
+		
+		return "start";
+	}
 	/**
 	 *
 	 *Handle the / endpoint
@@ -28,6 +70,14 @@ public class PlantPlacesController {
 	public String index(){
 		
 		return "start";
+	}
+
+	public SpecimenDTO getSpecimenDTO() {
+		return specimenDTO;
+	}
+
+	public void setSpecimenDTO(SpecimenDTO specimenDTO) {
+		this.specimenDTO = specimenDTO;
 	}
 }
 
